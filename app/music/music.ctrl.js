29f262ed257523e735e5098381053ce5dc3dfc09
Daumantas Girininkas <ssliteris@deloitte.com>
2017-08-17 00:11:42 +0300

    music update

--- app/music/music.ctrl.js
+++ app/music/music.ctrl.js
@@ -1,6 +1,60 @@
 (function () {
 
     'use strict';
-    angular.module('app.music', []).controller('musicCtrl', ['$scope', function($scope) { }]);
-    
+    angular.module('app.music', []).controller('musicCtrl', ['$scope', function ($scope) {
+
+
+        var songsArray = [
+            'abbaDancingQueen',
+            'aceOfBaseBeautifulLife',
+            'antisAlyvos',
+            'georgeMichaelCarelessWhisper',
+            'kellyFamilyAnAngel',
+            'marijonasMikutaviciusTrysMilijonai',
+            'poliarizuotiStiklaiMedziaiBeLapu',
+            'redHotChiliPeppersGiveItAway',
+            'stasysPovilaitisSvieskiManVel',
+            'theRollingStonesSatisfaction',
+            'twoUnlimitedNoLimit'
+        ];
+
+        var songs = {};
+        var songStarts = [
+            15,
+            15,
+            15,
+            15,
+            15,
+            15,
+            15,
+            15,
+            15,
+            15,
+            15000
+        ];
+
+        for (var song in songsArray) {
+            songs[songsArray[song]] = new Audio('app/sounds/songs/'+ songsArray[song] +'.mp3');
+
+            songs[songsArray[song]].addEventListener('load', function() {
+                console.log(song, 'loaded');
+                songs[songsArray[song]].currentTime = songStarts[song];
+
+                $scope[songsArray[song]] = function (rate) {
+                    songs[songsArray[song]].playbackRate = rate;
+                    console.log(songs[songsArray[song]].currentTime, songs);
+
+                    if (songs[songsArray[song]].currentTime > songStarts[song]) {
+                        songs[songsArray[song]].currentTime = songStarts[song];
+                        songs[songsArray[song]].pause();
+                    } else {
+                        songs[songsArray[song]].currentTime = songStarts[song];
+                        songs[songsArray[song]].play();
+                    }
+                };
+            }, true);
+
+        }
+    }]);
+
 })();
