d0732f6529cde149defaecf9c144b28014fba5af
ssliteris <ssliteris@deloitte.com>
2017-08-17 18:15:12 +0300

    songs

--- app/music/music.ctrl.js
+++ app/music/music.ctrl.js
@@ -21,7 +21,7 @@
 
         var songs = {};
         var songStarts = [
-            15,
+            30,
             15,
             15,
             15,
@@ -35,28 +35,226 @@
             15000
         ];
 
-        for (var song in songsArray) {
-            songs[songsArray[song]] = new Audio('app/sounds/songs/'+ songsArray[song] +'.mp3');
 
-            songs[songsArray[song]].addEventListener('load', function() {
-                console.log(song, 'loaded');
-                songs[songsArray[song]].currentTime = songStarts[song];
+            songs['abbaDancingQueen'] = new Audio('app/sounds/songs/abbaDancingQueen.mp3');
+            songs['aceOfBaseBeautifulLife'] = new Audio('app/sounds/songs/aceOfBaseBeautifulLife.mp3');
+            songs['antisAlyvos'] = new Audio('app/sounds/songs/antisAlyvos.mp3');
+            songs['georgeMichaelCarelessWhisper'] = new Audio('app/sounds/songs/georgeMichaelCarelessWhisper.mp3');
+            songs['kellyFamilyAnAngel'] = new Audio('app/sounds/songs/kellyFamilyAnAngel.mp3');
+            songs['marijonasMikutaviciusTrysMilijonai'] = new Audio('app/sounds/songs/marijonasMikutaviciusTrysMilijonai.mp3');
+            songs['metallicaEnterSandmanSmoothJazz'] = new Audio('app/sounds/songs/metallicaEnterSandmanSmoothJazz.mp3');
+            songs['poliarizuotiStiklaiMedziaiBeLapu'] = new Audio('app/sounds/songs/poliarizuotiStiklaiMedziaiBeLapu.mp3');
+            songs['redHotChiliPeppersGiveItAway'] = new Audio('app/sounds/songs/redHotChiliPeppersGiveItAway.mp3');
+            songs['stasysPovilaitisSvieskiManVel'] = new Audio('app/sounds/songs/stasysPovilaitisSvieskiManVel.mp3');
+            songs['theRollingStonesSatisfaction'] = new Audio('app/sounds/songs/theRollingStonesSatisfaction.mp3');
+            songs['twoUnlimitedNoLimit'] = new Audio('app/sounds/songs/twoUnlimitedNoLimit.mp3');
 
-                $scope[songsArray[song]] = function (rate) {
-                    songs[songsArray[song]].playbackRate = rate;
-                    console.log(songs[songsArray[song]].currentTime, songs);
+            songs['abbaDancingQueen'].onloadeddata = function() {
+                songs['abbaDancingQueen'].currentTime = songStarts[0];
+            };
 
-                    if (songs[songsArray[song]].currentTime > songStarts[song]) {
-                        songs[songsArray[song]].currentTime = songStarts[song];
-                        songs[songsArray[song]].pause();
-                    } else {
-                        songs[songsArray[song]].currentTime = songStarts[song];
-                        songs[songsArray[song]].play();
-                    }
-                };
-            }, true);
+            songs['aceOfBaseBeautifulLife'].onloadeddata = function() {
+                songs['aceOfBaseBeautifulLife'].currentTime = songStarts[1];
+            };
 
-        }
+            songs['antisAlyvos'].onloadeddata = function() {
+                songs['antisAlyvos'].currentTime = songStarts[2];
+            };
+
+            songs['georgeMichaelCarelessWhisper'].onloadeddata = function() {
+                songs['georgeMichaelCarelessWhisper'].currentTime = songStarts[3];
+            };
+
+            songs['kellyFamilyAnAngel'].onloadeddata = function() {
+                songs['kellyFamilyAnAngel'].currentTime = songStarts[4];
+            };
+
+            songs['marijonasMikutaviciusTrysMilijonai'].onloadeddata = function() {
+                songs['marijonasMikutaviciusTrysMilijonai'].currentTime = songStarts[5];
+            };
+
+            songs['metallicaEnterSandmanSmoothJazz'].onloadeddata = function() {
+                songs['metallicaEnterSandmanSmoothJazz'].currentTime = songStarts[6];
+            };
+
+            songs['poliarizuotiStiklaiMedziaiBeLapu'].onloadeddata = function() {
+                songs['poliarizuotiStiklaiMedziaiBeLapu'].currentTime = songStarts[7];
+            };
+
+            songs['redHotChiliPeppersGiveItAway'].onloadeddata = function() {
+                songs['redHotChiliPeppersGiveItAway'].currentTime = songStarts[8];
+            };
+
+            songs['stasysPovilaitisSvieskiManVel'].onloadeddata = function() {
+                songs['stasysPovilaitisSvieskiManVel'].currentTime = songStarts[9];
+            };
+
+            songs['theRollingStonesSatisfaction'].onloadeddata = function() {
+                songs['theRollingStonesSatisfaction'].currentTime = songStarts[10];
+            };
+
+            songs['twoUnlimitedNoLimit'].onloadeddata = function() {
+                songs['twoUnlimitedNoLimit'].currentTime = songStarts[11];
+            };
+
+            $scope['abbaDancingQueen'] = function (rate) {
+                var name = 'abbaDancingQueen';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[0]) {
+                    songs[name].currentTime = songStarts[0];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[0];
+                    songs[name].play();
+                }
+            };
+
+            $scope['aceOfBaseBeautifulLife'] = function (rate) {
+                var name = 'aceOfBaseBeautifulLife';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[1]) {
+                    songs[name].currentTime = songStarts[1];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[1];
+                    songs[name].play();
+                }
+            };
+
+            $scope['antisAlyvos'] = function (rate) {
+                var name = 'antisAlyvos';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[2]) {
+                    songs[name].currentTime = songStarts[2];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[2];
+                    songs[name].play();
+                }
+            };
+
+            $scope['georgeMichaelCarelessWhisper'] = function (rate) {
+                var name = 'georgeMichaelCarelessWhisper';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[3]) {
+                    songs[name].currentTime = songStarts[3];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[3];
+                    songs[name].play();
+                }
+            };
+
+            $scope['kellyFamilyAnAngel'] = function (rate) {
+                var name = 'kellyFamilyAnAngel';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[4]) {
+                    songs[name].currentTime = songStarts[4];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[4];
+                    songs[name].play();
+                }
+            };
+
+            $scope['marijonasMikutaviciusTrysMilijonai'] = function (rate) {
+                var name = 'marijonasMikutaviciusTrysMilijonai';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[5]) {
+                    songs[name].currentTime = songStarts[5];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[5];
+                    songs[name].play();
+                }
+            };
+
+            $scope['metallicaEnterSandmanSmoothJazz'] = function (rate) {
+                var name = 'metallicaEnterSandmanSmoothJazz';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[6]) {
+                    songs[name].currentTime = songStarts[6];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[6];
+                    songs[name].play();
+                }
+            };
+
+            $scope['poliarizuotiStiklaiMedziaiBeLapu'] = function (rate) {
+                var name = 'poliarizuotiStiklaiMedziaiBeLapu';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[7]) {
+                    songs[name].currentTime = songStarts[7];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[7];
+                    songs[name].play();
+                }
+            };
+
+            $scope['redHotChiliPeppersGiveItAway'] = function (rate) {
+                var name = 'redHotChiliPeppersGiveItAway';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[8]) {
+                    songs[name].currentTime = songStarts[8];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[8];
+                    songs[name].play();
+                }
+            };
+
+            $scope['stasysPovilaitisSvieskiManVel'] = function (rate) {
+                var name = 'stasysPovilaitisSvieskiManVel';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[9]) {
+                    songs[name].currentTime = songStarts[9];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[9];
+                    songs[name].play();
+                }
+            };
+
+            $scope['theRollingStonesSatisfaction'] = function (rate) {
+                var name = 'theRollingStonesSatisfaction';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[10]) {
+                    songs[name].currentTime = songStarts[10];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[10];
+                    songs[name].play();
+                }
+            };
+
+            $scope['twoUnlimitedNoLimit'] = function (rate) {
+                var name = 'twoUnlimitedNoLimit';
+                songs[name].playbackRate = rate;
+
+                if (songs[name].currentTime > songStarts[11]) {
+                    songs[name].currentTime = songStarts[11];
+                    songs[name].pause();
+                } else {
+                    songs[name].currentTime = songStarts[11];
+                    songs[name].play();
+                }
+            };
+
+            
+
     }]);
 
 })();
